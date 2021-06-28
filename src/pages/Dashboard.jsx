4aa4e38d72f992321458734487239fd70411d5ee
Utkarsh Tiwari <utkarsh.tiwari@somaiya.edu>
2021-06-28 15:29:30 +0000

    added GODgit add .!

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -60,7 +60,7 @@ const Dashboard = () => {
           </main>
         </div>
         <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-          <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="" />
+          <img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://sadanduseless.b-cdn.net/wp-content/uploads/2018/10/ikea-shark6.jpg" alt="Plushie" />
         </div>
         {/* <div className="m-5 py-5">
           <button
